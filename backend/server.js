d5e7ab9ad956a243b16b0986ea7940d4850801b3
Kaverisha Bhakat <mistum160303@gmail.com>
2026-08-08 12:48:16 +0530

    Set up backend and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.get("/",(req,res)=>{
+    res.send("CampusEvent Backend is running");
+});
+
+app.listen(PORT ,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
