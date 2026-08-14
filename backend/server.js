1fb43fe74f97f4fdbede931d29760921905f6b6a
Kaverisha Bhakat <mistum160303@gmail.com>
2026-08-14 23:51:39 +0530

    Fix JSON middleware for event API

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const eventRoutes = require("./routes/eventRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 
 app.use("/api/events", eventRoutes);
 
@@ -21,4 +22,3 @@ app.listen(PORT ,()=>{
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.use(express.json());
