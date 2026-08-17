1f13a44610fc92d84cacd4bbb731751ec8347aab
Kaverisha Bhakat <mistum160303@gmail.com>
2026-08-17 23:45:20 +0530

    Add create event API

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -16,4 +16,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const event = await Event.create(req.body);
+
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to create event",
+      error: error.message,
+    });
+  }
+});
+
 module.exports = router;
